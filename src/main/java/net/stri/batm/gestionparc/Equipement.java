7f3c2aa3a73f6c3c1bb985a206a1bd298eeb2a5b
VincentBalat <balatvincent31@gmail.com>
2015-05-06 09:50:06 +0200

    Import des fichiers de l'ancienne verison du projet

---
+++ src/main/java/net/stri/batm/gestionparc/Equipement.java
@@ -0,0 +1,16 @@
+package net.stri.batm.gestionparc;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Vince
+ */
+public class Equipement {
+    
+}
