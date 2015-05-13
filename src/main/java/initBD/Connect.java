2992c1c337818a856578d4dfeda38a0b27e9a25a
gasparmeyerfeld <gmeyerfeld@gmail.com>
2015-05-13 15:59:10 +0200

    Ajout du programme de création de la base de données et des tables.

--- src/main/java/initBD/Connect.java
+++ src/main/java/initBD/Connect.java
@@ -25,7 +25,7 @@ public class Connect {
     try {
       Class.forName("org.postgresql.Driver");
          
-      String url = "jdbc:postgresql://localhost:5432";
+      String url = "jdbc:postgresql://localhost:5432/gestionpark";
       String user = "postgres";
       String passwd = "postgres";
          
@@ -33,43 +33,31 @@ public class Connect {
          
       //Création d'un objet Statement
       Statement state = conn.createStatement();
-      //L'objet ResultSet contient le résultat de la requête SQL
-      ResultSet db = state.executeQuery("CREATE DATABASE Gestionpark\n" +
-"  WITH OWNER = postgres\n" +
-"       ENCODING = 'UTF8'\n" +
-"       TABLESPACE = pg_default\n" +
-"       LC_COLLATE = 'French_France.1252'\n" +
-"       LC_CTYPE = 'French_France.1252'\n" +
-"       CONNECTION LIMIT = -1;");
-      //On récupère les MetaData
-      ResultSetMetaData dbMeta = db.getMetaData();
 
-      db.close();
+      state.execute("DROP TABLE if exists Interfaces ;");
+      state.execute("DROP TABLE if exists Equipements ;");
+      state.execute("DROP TABLE if exists Salles ;");
+      state.execute("DROP TABLE if exists Batiments ;");
+      state.close();
+      state = conn.createStatement();
+     /* ResultSet batiments = */
+      state.execute("CREATE TABLE Batiments\n" +
+"       (IdBatiment     INT CONSTRAINT PK_IdBatiment PRIMARY KEY,\n" +
+"        NomB           VARCHAR(255),\n" +
+"        NumB           INT);\n");
+
+      state = conn.createStatement();
+      state.execute("CREATE TABLE Salles\n" +
+"       (IdSalle    INT CONSTRAINT PK_IdSalle PRIMARY KEY,\n" +
+"        NomS       VARCHAR(255),\n" +
+"        NumS       INT,\n" +
+"        IdBat      INT CONSTRAINT FK_IdBat REFERENCES Batiments(IdBatiment));\n");
       
-      ResultSet batiments = state.executeQuery("CREATE TABLE Batiments\n" +
-"       (IdBatiment INT CONSTRAINT PK_IdBatiment PRIMARY KEY,\n" +
-"        NomB   VARCHAR(255),\n" +
-"        NumB   INT);\n");
-      
-      //On récupère les MetaData
-      ResultSetMetaData batimentsMeta = batiments.getMetaData();
-      
-      ResultSet salles = state.executeQuery("CREATE TABLE Salles\n" +
-"       (IdSalle INT CONSTRAINT PK_IdSalle PRIMARY KEY,\n" +
-"        NomS   VARCHAR(255),\n" +
-"        NumS   INT,\n" +
-"        IdBat  INT CONSTRAINT FK_IdBat REFERENCES Batiments(IdBatiment));\n");
-      
-      //On récupère les MetaData
-      ResultSetMetaData sallesMeta = salles.getMetaData();
-      
-      salles.close();
-      
-      ResultSet equipements = state.executeQuery("CREATE TABLE Equipements\n" +
+      state.execute("CREATE TABLE Equipements\n" +
 "       (IdEquipement INT CONSTRAINT PK_IdEquipement PRIMARY KEY,\n" +
 "        Marque     VARCHAR(255),\n" +
 "        NomEq      VARCHAR(255),\n" +
-"        NumEq      INT\n" +
+"        NumEq      INT,\n" +
 "        Type       VARCHAR(255),\n" +
 "        Active     BOOLEAN,\n" +
 "        Processeur VARCHAR(255),\n" +
@@ -77,21 +65,13 @@ public class Connect {
 "        Disque     INT,\n" +
 "        IdSalle    INT CONSTRAINT FK_IdSalle REFERENCES Salles(IdSalle));\n");
       
-      //On récupère les MetaData
-      ResultSetMetaData equipementsMeta = equipements.getMetaData();
-      
-      equipements.close();
-      
-      ResultSet interfaces = state.executeQuery("CREATE TABLE Interfaces\n" +
+      state.execute("CREATE TABLE Interfaces\n" +
 "       (Mac VARCHAR(255) CONSTRAINT PK_Mac PRIMARY KEY,\n" +
-"        Ip     VARCHAR(255),\n" +
-"        NomInt      VARCHAR(255),\n" +
-"        Vitesse      INT\n" +
-"        IdEq    INT CONSTRAINT FK_IdEq REFERENCES Equipements(IdEquipement));\n");
-      
-      //On récupère les MetaData
-      ResultSetMetaData interfaceMeta = equipements.getMetaData();
-      
+"        Ip         VARCHAR(255),\n" +
+"        NomInt     VARCHAR(255),\n" +
+"        Vitesse    INT,\n" +
+"        IdEq       INT CONSTRAINT FK_IdEq REFERENCES Equipements(IdEquipement));\n");
+
       state.close();
          
     } catch (Exception e) {
